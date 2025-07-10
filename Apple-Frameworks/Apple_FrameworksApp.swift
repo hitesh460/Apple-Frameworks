3de558964112e91eaf4cfa869f5c4921c45bd956
Hitesh Sahu <sahu.hitesh460@gmail.com>
2025-07-10 19:37:37 +0530

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Hitesh Jayaram Sahu on 10/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
